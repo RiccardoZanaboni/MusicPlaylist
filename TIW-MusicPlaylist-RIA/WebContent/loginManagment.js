8c66d11dccf21f1ba1fa1f2ab82097b3508aa5b7
Feb <fabio.rossanigo01@universitadipavia.it>
2021-07-05 11:08:53 +0200

    Added the login (very base version) to the RIA application

---
+++ TIW-MusicPlaylist-RIA/WebContent/loginManagment.js
@@ -0,0 +1,33 @@
+// js used for the managment of the login
+
+(function(){
+    document.getElementById("login_button").addEventListener('click', (e) =>{
+        var form = e.target.closest("form");
+        if(form.checkValidity()){
+            makeCall("POST", "CheckLogin", e.target.closest("form"),
+            function(x){
+                if(x.readyState == XMLHttpRequest.DONE){
+                    var message = x.responseText;
+                    switch(x.status){
+                        case 200:
+                            sessionStorage.setItem('username', message);
+                            window.location.href = "Home.html";
+                            break
+                        case 400: // bad request
+                            document.getElementById("errormessage").textContent = message;
+                            break;
+                        case 401: // unauthorized
+                              document.getElementById("errormessage").textContent = message;
+                              break;
+                        case 500: // server error
+                            document.getElementById("errormessage").textContent = message;
+                            break;
+                        }
+                    }
+                }
+            );
+        } else{
+            form.reportValidity();
+        }
+    })
+})();
